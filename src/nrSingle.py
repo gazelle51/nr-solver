128ba8df6e023f348523ac52b25c08640ed0e280
gazelle51 <sarahgazelle@hotmail.com>
2020-05-15 11:56:05 +1000

    Update nrSingle.py

--- src/nrSingle.py
+++ src/nrSingle.py
@@ -1,21 +1,27 @@
 # NR Method
 def newtonRaphsonSingleVariable(x, f, df, eps=0.0001):
-    print("Starting Newton Raphson Method for a single variable")
-
+    # Initialisation
     delta = f(x) / df(x)
     i = 0
 
+    print("Starting Newton Raphson Method for a single variable")
     print("\nIteration |       x (7dp)")
     print("==========================")
+
+    # Start looping
     while abs(delta) >= eps:
         print("    {:5d} | {: 13.7f}".format(i, x))
 
+        # Calculote delta
         delta = f(x) / df(x)
 
-        # x(i+1) = x(i) - f(x) / f'(x)
+        # Apply update to the root
         x = x - delta
 
+        # Increment counter
         i = i + 1
 
     print("    {:5d} | {: 13.7f}".format(i, x))
     print("\nThe value of the root is %.4f after %d iterations" % (x, i))
+
+    return x
