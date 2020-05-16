0c4db954e0a1d743e93683d5f662ec3b9b6acc8e
gazelle51 <sarahgazelle@hotmail.com>
2020-05-16 12:19:13 +1000

    two var method ui

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class MyApp:
 
         self.header()
         self.nrSingle()
+        self.nrTwo()
         self.footer()
 
     def header(self):
@@ -22,7 +23,7 @@ class MyApp:
         ).grid(row=0, pady=10)
 
     def nrSingle(self):
-        singleTitle = tk.Label(
+        tk.Label(
             self.root,
             text="Single Variable Method",
             font=("Helvetica", 15, "bold"),
@@ -35,9 +36,11 @@ class MyApp:
         self.eqSingle = tk.Entry(self.root)
         self.eqSingle.insert(0, "15*((x-4)/7) + (sin(5*x))**3")
         self.eqSingle.grid(row=2, column=1)
+
         self.varSingle = tk.Entry(self.root)
         self.varSingle.insert(0, "x")
         self.varSingle.grid(row=3, column=1)
+
         self.initGuessSingle = tk.Entry(self.root)
         self.initGuessSingle.insert(0, 2)
         self.initGuessSingle.grid(row=4, column=1)
@@ -46,10 +49,53 @@ class MyApp:
             self.root, text="GO", fg="red", command=self.runNrSingle
         ).grid(row=5)
 
+    def nrTwo(self):
+        tk.Label(
+            self.root,
+            text="Two Variable Method",
+            font=("Helvetica", 15, "bold"),
+        ).grid(row=6)
+
+        tk.Label(self.root, text="Equation 1").grid(row=7)
+        tk.Label(self.root, text="Equation 2").grid(row=8)
+        tk.Label(self.root, text="Variable 1").grid(row=9)
+        tk.Label(self.root, text="Variable 2").grid(row=10)
+        tk.Label(self.root, text="Initial guess for variable 1").grid(row=11)
+        tk.Label(self.root, text="Initial guess for variable 2").grid(row=12)
+
+        self.eq1Two = tk.Entry(self.root)
+        self.eq1Two.insert(0, "10*x*sin(y) + 0.5")
+        self.eq1Two.grid(row=7, column=1)
+
+        self.eq2Two = tk.Entry(self.root)
+        self.eq2Two.insert(0, "10*x**2 - 10*x*cos(y) + 0.2")
+        self.eq2Two.grid(row=8, column=1)
+
+        self.var1Two = tk.Entry(self.root)
+        self.var1Two.insert(0, "x")
+        self.var1Two.grid(row=9, column=1)
+
+        self.var2Two = tk.Entry(self.root)
+        self.var2Two.insert(0, "y")
+        self.var2Two.grid(row=10, column=1)
+
+        self.initGuess1Two = tk.Entry(self.root)
+        self.initGuess1Two.insert(0, 1)
+        self.initGuess1Two.grid(row=11, column=1)
+
+        self.initGuess2Two = tk.Entry(self.root)
+        self.initGuess2Two.insert(0, 0)
+        self.initGuess2Two.grid(row=12, column=1)
+
+        tk.Button(self.root, text="GO", fg="red", command=self.runNrTwo).grid(
+            row=13
+        )
+
     def footer(self):
-        tk.Button(self.root, text="QUIT", fg="red", command=quit).grid(row=6)
+        tk.Button(self.root, text="QUIT", fg="red", command=quit).grid(row=14)
 
     def runNrSingle(self):
+        # TODO: add epsilon
         nrSingle.newtonRaphsonSingleVariable(
             self.eqSingle.get(),
             self.varSingle.get(),
@@ -57,10 +103,14 @@ class MyApp:
         )
 
     def runNrTwo(self):
+        # TODO: add epsilon
         nrTwo.newtonRaphsonTwoVariables(
-            self.eqSingle.get(),
-            self.varSingle.get(),
-            float(self.initGuessSingle.get()),
+            self.eq1Two.get(),
+            self.eq2Two.get(),
+            self.var1Two.get(),
+            self.var2Two.get(),
+            float(self.initGuess1Two.get()),
+            float(self.initGuess2Two.get()),
         )
 
 
