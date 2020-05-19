16d8418821eb95f093ef476ed6a107b74116035b
gazelle51 <sarahgazelle@hotmail.com>
2020-05-19 15:34:41 +1000

    updates

--- setup.py
+++ setup.py
@@ -3,18 +3,30 @@ import os.path
 import sys
 
 
+PYTHON_INSTALL_DIR = os.path.dirname(sys.executable)
+# os.environ["TCL_LIBRARY"] = os.path.join(PYTHON_INSTALL_DIR, "tcl", "tcl8.6")
+# os.environ["TK_LIBRARY"] = os.path.join(PYTHON_INSTALL_DIR, "tcl", "tk8.6")
+
 # Dependencies are automatically detected, but it might need
 # fine tuning.
 buildOptions = dict(
-    packages=[],
-    includes=["numpy", "os", "sympy", "sys", "tkinter"],
+    packages=["numpy", "os", "sympy", "sys", "tkinter"],
+    includes=[],
     excludes=[],
-    include_files=["src/",],
-    replace_paths=[("*", "")],
-    path=sys.path + ["src"],
+    include_files=[
+        # "src/",
+        # (
+        #     os.path.join(PYTHON_INSTALL_DIR, "DLLs", "tk86t.dll"),
+        #     os.path.join("lib", "tk86t.dll"),
+        # ),
+        # (
+        #     os.path.join(PYTHON_INSTALL_DIR, "DLLs", "tcl86t.dll"),
+        #     os.path.join("lib", "tcl86t.dll"),
+        # ),
+    ],
+    # replace_paths=[("*", "")],
+    path=sys.path + ["lib"],
 )
-
-import sys
 
 base = "Win32GUI" if sys.platform == "win32" else None
 
